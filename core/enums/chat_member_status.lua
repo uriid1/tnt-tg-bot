390c8088d843ea4f61c79dcf36441388c2e11bee
uriid1 <appdurov@gmail.com>
2023-05-08 05:18:06 +0300

    Add new enums

---
+++ core/enums/chat_member_status.lua
@@ -0,0 +1,19 @@
+--[[
+    ####--------------------------------####
+    #--# Author:   by uriid1            #--#
+    #--# License:  GNU GPLv3            #--#
+    #--# Telegram: @main_moderator      #--#
+    #--# E-mail:   appdurov@gmail.com   #--#
+    ####--------------------------------####
+--]]
+
+local chat_member_status = {
+    CREATOR = 'creator';
+    ADMINISTRATOR = 'administrator';
+    MEMBER = 'member';
+    RESTRICTED = 'restricted';
+    LEFT = 'left';
+    KICKED = 'kicked';
+}
+
+return chat_member_status
---
+++ core/enums/parse_mode.lua
@@ -0,0 +1,16 @@
+--[[
+    ####--------------------------------####
+    #--# Author:   by uriid1            #--#
+    #--# License:  GNU GPLv3            #--#
+    #--# Telegram: @main_moderator      #--#
+    #--# E-mail:   appdurov@gmail.com   #--#
+    ####--------------------------------####
+--]]
+
+local parse_mode = {
+    HTML = 'HTML';
+    MARKDOWN = 'Markdown';
+    MARKDOWN_V2 = 'MarkdownV2';
+}
+
+return parse_mode
