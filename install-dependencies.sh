50cc8ea114a610610a9a0008a5c18e9e154ac47a
uriid1 <appdurov@gmail.com>
2024-07-20 18:59:51 +0300

    Update deps and .gitignore

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -5,4 +5,4 @@ tarantoolctl rocks install --server=https://rocks.tarantool.org/ --local http
 tarantoolctl rocks install --server=https://luarocks.org lua-multipart-post 1.0-0
 
 # Optional
-tarantoolctl rocks install --server=https://luarocks.org pimp 1.7-0
+# tarantoolctl rocks install --server=https://luarocks.org pimp
