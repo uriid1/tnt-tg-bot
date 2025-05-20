848504aa9d3f576d211449aea7b01cf85fb34424
uriid1 <appdurov@gmail.com>
2025-05-20 13:20:10 +0300

    Bump 1.0.2

--- bot/init.lua
+++ bot/init.lua
@@ -4,7 +4,7 @@
 --- Licence MIT
 -----------------------------------
 --- @module bot
-local bot = { _version = '1.0.0' }
+local bot = { _version = '1.0.2' }
 
 package.path = package.path .. ';.rocks/share/lua/5.1/?.lua'
 package.cpath = package.cpath .. ';.rocks/lib/lua/5.1/?.so'
