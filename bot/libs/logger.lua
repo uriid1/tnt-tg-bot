8c91974e8d152951f93b0b4912b6c86a6995ae16
uriid1 <appdurov@gmail.com>
2025-11-26 18:59:00 +0300

    Logger wrapper rewritten

--- bot/libs/logger.lua
+++ bot/libs/logger.lua
@@ -1,56 +1,51 @@
---- Default tnt bot logger
+--- Default bot logger
 --
-local json = require('json')
-local log = require('log')
-local colors = require('bot.ext.colors')
+local defaultLogger = require('log')
 
-local function colorize(color, text)
-  if colors.xterm256color == false then
-    return text
+local current = defaultLogger
+
+local wrapper = {}
+
+function wrapper.init(pkg)
+  if pkg then
+    current = pkg
+  else
+    current = defaultLogger
   end
-  return color .. text .. colors.reset
+  return wrapper
 end
 
-local logger = {
-  cfg = log.cfg
-}
-
--- luacheck: ignore unpack
--- luacheck: ignore table
-local unpack = table.unpack or unpack
-local function wrapArgs(...)
-  local t = {}
-  for _,value in ipairs({...}) do
-    table.insert(t, colorize(colors.brightWhite, tostring(value)))
+function wrapper.cfg(opts)
+  if current.cfg then
+    return current.cfg(opts)
   end
-  return unpack(t)
 end
 
-function logger.info(text, ...)
-  local isTable = type(text) == 'table'
-  log.info(colorize(colors.brightGreen, isTable and json.encode(text) or text), wrapArgs(...))
+function wrapper.info(...)
+  return current.info(...)
 end
 
-function logger.verbose(text, ...)
-  local isTable = type(text) == 'table'
-  log.verbose(colorize(colors.brightBlue, isTable and json.encode(text) or text), wrapArgs(...))
+function wrapper.verbose(...)
+  return current.verbose(...)
 end
 
-function logger.warn(text, ...)
-  local isTable = type(text) == 'table'
-  log.warn(colorize(colors.brightOrange, isTable and json.encode(text) or text), wrapArgs(...))
+function wrapper.debug(...)
+  return current.debug and current.debug(...) or current.verbose
 end
 
-function logger.error(text, ...)
-  local isTable = type(text) == 'table'
-  log.error(colorize(colors.brightRed, isTable and json.encode(text) or text), wrapArgs(...))
+function wrapper.warn(...)
+  return current.warn(...)
+end
+
+function wrapper.error(...)
+  return current.error(...)
 end
 
 -- Test
--- logger.info("[info] %s | %s", 'GET', '200')
--- logger.info({ message = 'Hello from logger' })
--- logger.verbose("[verbose] %s | %s", 'command', '/start')
--- logger.warn("[warn] %s | %s", 'event', 'user 1234 not found')
--- logger.error("[error] %s | %s", 'fatal', 'function foo.bar, line 123')
+-- log.info("[info] %s | %s", 'GET', '200')
+-- log.info({ message = 'Hello from logger' })
+-- log.verbose("[verbose] %s | %s", 'command', '/start')
+-- log.warn("[warn] %s | %s", 'event', 'user 1234 not found')
+-- log.error("[error] %s | %s", 'fatal', 'function foo.bar, line 123')
 
-return logger
+return wrapper
